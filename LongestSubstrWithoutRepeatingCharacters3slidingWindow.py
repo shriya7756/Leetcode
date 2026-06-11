21fe756192d35b933d68e17bb3fd5eadfec76d32
shriya7756 <shriyapachunuri@gmail.com>
2026-06-12 00:22:57 +0530

    added LC3slidingwindow code
    Files: LongestSubstrWithoutRepeatingCharacters3slidingWindow.py

---
+++ LongestSubstrWithoutRepeatingCharacters3slidingWindow.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        i=0
+        res=0
+        Set=set()
+        for j in range(len(s)):
+            while s[j] in Set:
+                Set.remove(s[i])
+                i=i+1
+            Set.add(s[j])
+            res=max(res,j-i+1)
+            
+        return res

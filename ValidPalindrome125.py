3be27e3cf41df0431c4bf87a03a5aa32e4218c5b
shriya7756 <shriyapachunuri@gmail.com>
2026-06-12 00:14:57 +0530

    added 1 file to leetcode repo
    Files: ValidPalindrome125.py

---
+++ ValidPalindrome125.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        f=""
+        for i in s:
+            if i.isalnum():
+                f+=i.upper()
+        l=0
+        r=len(f)-1
+        while l<r:
+          if f[l]!=f[r]:
+            return False
+          l=l+1
+          r=r-1
+        return True
+            
+s=Solution()
+print(s.isPalindrome(s = "A man, a plan, a canal: Panama"))
+            

a6ea850ebd3087973b8867fe274982d4699f8c32
shriya7756 <shriyapachunuri@gmail.com>
2026-06-12 00:19:57 +0530

    added 121 code to Leetcode
    Files: Stock121bruteforce.py

---
+++ Stock121bruteforce.py
@@ -0,0 +1,13 @@
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+      res=0
+      for i in range(len(prices)-1):
+          buy=min(prices[:i+1])
+          
+          for j in range(i+1,len(prices)-1):
+              sell=max(prices[i+1:j+1])
+              res=max(res,sell-buy)
+      return res
+s=Solution()
+print(s.maxProfit([7,1,5,3,6,4]))
